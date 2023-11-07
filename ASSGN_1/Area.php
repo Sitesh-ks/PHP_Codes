65f0c1465c0cfefdfcde553a11961ee8153e5b67
Sitesh Kumar Samal <112224704+Sitesh-ks@users.noreply.github.com>
2023-11-07 09:38:42 +0530

    Add files via upload

---
+++ ASSGN_1/Area.php
@@ -0,0 +1,31 @@
+<?php
+// area of traingle
+$base=10;
+$height=10;
+echo "Area of triangle : ". 1/2*($base*$height)."<br>";
+
+// area of square
+$side= 10;
+echo "Area of square : ". ($side*$side)."<br>";
+
+// area of circle
+$radius=10;
+$pi=3.1479;
+echo "Area of circle : ".$pi*($radius*$radius)."<br>";
+
+// area of rectangle
+$length=10;
+$breadth=20;
+echo"Area of rectangle : ".($length*$breadth)."<br>";
+
+//area of parallelogram
+$p1=10;
+$p2=30;
+echo "area of parallelogram :". 1/2*($p1+$p2) ."<br>";
+
+//area of rhombus
+$d1=5;
+$d2=4;
+echo "area of rhombus : ". 1/2*($d1+$d2); 
+
+?>
---
+++ ASSGN_1/TempConverter.php
@@ -0,0 +1,4 @@
+<?php
+$centigrade=50;  //--->Temp in celcius
+echo "Temp in farenheit is : ".(($centigrade * 9/5) + 32 );
+?>
