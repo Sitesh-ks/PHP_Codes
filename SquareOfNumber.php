1bd3119bd7784347ac8863b3a641cd383d49c59e
Sitesh Kumar Samal <112224704+Sitesh-ks@users.noreply.github.com>
2023-11-07 09:39:07 +0530

    Delete SquareOfNumber.php

--- SquareOfNumber.php
+++
@@ -1,9 +0,0 @@
-<?php
-function square($number) {
-    return $number * $number;
-}
-
-$number = 5; // change the value of number for desired uotput
-$result = square($number);
-echo "Square of " . $number . ": " . $result;
-?>
