2df3acb4e9ed13771326a128a6c4c15d519387a9
Sitesh Kumar Samal <112224704+Sitesh-ks@users.noreply.github.com>
2023-11-07 09:38:59 +0530

    Delete Details.php

--- Details.php
+++
@@ -1,9 +0,0 @@
-<?php
-function printDetails($name, $branch, $regNo) {
-    echo "Name: " . $name . "<br>";
-    echo "Branch: " . $branch . "<br>";
-    echo "Registration No: " . $regNo . "<br>";
-}
-
-printDetails("Sitesh Kumar Samal", "CSIT", "2241019264");
-?>
